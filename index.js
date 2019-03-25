8e4925e0398d09053f2d396c8260b9263fda06c4
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-25 12:40:47 -0400

    changed index.js to address Heroku issues

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const { exec } = require('child_process');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-// Set port (default: 3000). For Heroku, we need to use
-// the port set by the environment variable $PORT
-const port = process.env.PORT || 3001;
-
-const command = `json-server ./db.json --port ${port}`;
-
-exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.log('Error running exec', err);
-    return;
-  }
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
-});
+server.use(middlewares);
+server.use(router);
+server.listen(port);
